7238bb1e68eb78cc1cb8cace03c7ab81f37e14ea
Wesley Martins <wesleysbmartins@gmail.com>
2024-11-16 12:59:38 -0300

    [ADD] message api

--- src/request_handler/index.js
+++ src/request_handler/index.js
@@ -3,7 +3,7 @@ const { print } = require("../print/index.js");
 function requestHandler(req, res) {
     print(`request received - ${new Date().toISOString()}`);
     res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify("Server Running...JS"));
+    return res.end(JSON.stringify("Hello World!"));
 }
 
 module.exports = { requestHandler };
