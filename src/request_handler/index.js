b8b388244038a60e26cf195637348376369acd55
Wesley Martins <wesleysbmartins@gmail.com>
2024-11-22 14:04:50 -0300

    [TEST] tesntando pipeline na aws

--- src/request_handler/index.js
+++ src/request_handler/index.js
@@ -3,7 +3,22 @@ const { print } = require("../print/index.js");
 function requestHandler(req, res) {
     print(`request received - ${new Date().toISOString()}`);
     res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify("Siiiiuu!"));
+    return res.end(JSON.stringify(`
+    ⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⡀
+    ⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇
+    ⠀⠀⠀⠀⠀⣀⣤⣾⣿⣟
+    ⠀⠀⠀⠀⣾⣿⡿⠿⠿⠿⣿⣦
+    ⠀⠀⠀⢸⣿⣿⡇⢠⡄⢀⣿⣿⡄
+    ⠀⠀⠀⣿⡟⣿⣿⣿⠃⣸⣿⣿⣧
+    ⠀⠀⢸⣿⠁⣸⣿⡟⠀⣿⣿⡌⢿⣧
+    ⠀⠀⣾⡇⠀⣿⣿⣃⣸⣿⣿⣿⠈⠻⣷⣄⡀
+    ⠀⢠⡟⠀⢠⣿⣿⣿⣿⣿⣿⣿⣧⠀⠈⢿⡦
+    ⠀⠀⠀⠀⣾⣿⣿⣿⠛⠛⣿⣿⣿⣧
+    ⠀⠀⠀⢸⣿⡿⠋⠁⠀⠀⠀⠈⠻⣿⣿⡄
+    ⠀⠀⣰⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣧
+    ⠀⣼⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣧
+    ⣸⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣇`,
+));
 }
 
 module.exports = { requestHandler };
